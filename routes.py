5696438819cabbcba1c52fdf6ac88b21060440f0
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-18 12:17:25 -0300

    config: add jwt_acess_token_expires

--- routes.py
+++ routes.py
@@ -7,7 +7,9 @@ from src.controllers.login_controller import login_bp
 def create_app():
     app = Flask(__name__)
 
-    app.config["JWT_SECRET_KEY"] = "1234"  # Recomendado armazenar essas informações em um arquivo .env
+    # Recomendado armazenar essas informações em um arquivo .env
+    app.config["JWT_SECRET_KEY"] = "1234"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 3600
     jwt = JWTManager(app)
 
     app.register_blueprint(user_bp, url_prefix="/user")
